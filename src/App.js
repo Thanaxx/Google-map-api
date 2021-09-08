21e93f714d2b0af8c6082d41ed84c9823a6b2ff5
Thana Ernaline <abordajechristeljoy@gmail.com>
2021-09-08 14:13:56 +0800

    map-api

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+import Map from './components/Map';
+import './App.css'
+import Loading from './components/Loading'
+
+function App() {
+
+  const [eventData, setEventData] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const fetchEvents = async () =>{
+      setLoading(true)
+      const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events')
+      const {events} = await res.json()
+
+      setEventData(events)
+      setLoading(false)
+    }
+
+    fetchEvents()
+  },[])
+
+  return (
+    <>
+        {!loading ? <Map eventData={eventData} /> : <Loading /> }
+    </>
+  )
+}
+
+export default App;
